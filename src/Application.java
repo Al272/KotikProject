2342f64954f3fc2b023f29d1140f7dd2ede9eb4c
Al272 <54018766+Al272@users.noreply.github.com>
2021-03-05 15:10:14 +0300

    ver.1

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+import model.Kotik;
+
+public class Application {
+    public static void main(String[] args) {
+        Kotik streetCat = new Kotik();
+        Kotik homeCat = new Kotik(10,18,"Barsik","meeeow");
+        homeCat.liveAnotherDay();
+        System.out.println("My name is - '"+homeCat.getName()+"' and my weight - "+homeCat.getWeight());
+        System.out.println("MeowCompareEquals = "+streetCat.getMeuw().equalsIgnoreCase(homeCat.getMeuw()));
+        System.out.println("MeowComapre'==' = "+streetCat.getMeuw().equalsIgnoreCase(homeCat.getMeuw()));
+        System.out.println("Total amoutn of Kotik = "+Kotik.getAmountKotik());
+    }
+}
---
+++ src/model/Kotik.java
@@ -0,0 +1,150 @@
+package model;
+
+public class Kotik {
+    private int prettiness;
+    private int weight;
+    private String name;
+    private String meuw;
+
+    private static int amountKotik;
+
+    public Kotik(int prettiness, int weight, String name, String meuw) {
+        this.prettiness = prettiness;
+        this.weight = weight;
+        this.name = name;
+        this.meuw = meuw;
+        ++amountKotik;
+    }
+    public Kotik(){
+        //dafault Kotik
+        setPrettiness(1);
+        setWeight(5);
+        setName("streetKotik");
+        setMeuw("meow");
+        ++amountKotik;
+    }
+
+    public int getPrettiness() {
+        return prettiness;
+    }
+
+    private void setPrettiness(int prettiness) {
+        this.prettiness += prettiness;
+
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        if(weight>0 && weight<20)
+        this.weight = weight;
+        else
+            System.out.println("Set correct weight!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty() || name.length()<2)
+            System.out.println("Set correct name!");
+        else
+            this.name = name;
+    }
+
+    public String getMeuw() {
+        return meuw;
+    }
+
+    public void setMeuw(String meuw) {
+        this.meuw = meuw;
+    }
+
+    public static int getAmountKotik() {
+        return amountKotik;
+    }
+    public boolean wantEat(){
+        System.out.println("I want eat!!!");
+        return false;
+    }
+    public boolean sleep() {
+        if (getPrettiness()> 0) {
+            setPrettiness(-1);
+            System.out.println("Kotik sleep");
+            return true;
+        } else
+           return wantEat();
+    }
+    public boolean walk () {
+        if (getPrettiness() > 0) {
+            setPrettiness(-2);
+            System.out.println("Kotik walk");
+            return true;
+        } else
+                return wantEat();
+    }
+        public boolean play () {
+            if (getPrettiness() > 0) {
+                setPrettiness(-3);
+                System.out.println("Kotik play");
+                return true;
+            } else
+                return wantEat();
+        }
+        public boolean catchMouse () {
+            if (getPrettiness() > 0) {
+                setPrettiness(-3);
+                System.out.println("Kotik catch mouse");
+                return true;
+            } else
+                return wantEat();
+        }
+        public boolean runAwayFromDog () {
+            if (getPrettiness() > 0) {
+                setPrettiness(-4);
+                System.out.println("Kotik run away from dog");
+                return true;
+            } else
+                return wantEat();
+        }
+        public void eat(int prettinessAmout){
+            System.out.println("Kotik eat");
+            setPrettiness(prettinessAmout);
+        }
+    public void eat(int prettinessAmout,String food){
+        System.out.println("Kotik eat "+food);
+        setPrettiness(prettinessAmout);
+    }
+    public void eat(){
+       //default eat
+        eat(1,"kitiKat");
+    }
+    public void liveAnotherDay(){
+        for(int i=0; i<24; i++){
+            int random = (int)(Math.random()*5)+1;
+            boolean condition = false;
+            switch (random){
+                case 1 : if(!sleep())
+                    eat();
+                break;
+                case 2 : if(!walk())
+                    eat(random*2,"Fish");
+                break;
+                case 3 : if(!play())
+                    eat(random*2);
+                break;
+                case 4 : if(!catchMouse())
+                    eat(random,"Chicken");
+                break;
+                case 5 : if(!runAwayFromDog())
+                    eat(random-2);
+                break;
+                default:
+                    System.out.println("number in switch is not valid");
+            }
+        }
+    }
+}
